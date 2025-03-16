b9f27777d7ada20f8d79791f75ae94f6824e703e
vpow10 <jan.szymczak10@gmail.com>
2025-03-16 14:22:15 +0100

    Create wallet model compatible with gorm

--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module token-transfer-api
 go 1.24.1
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
--- models/wallet.go
+++ models/wallet.go
@@ -1,6 +1,30 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Wallet struct {
-	Address string
-	Balance int
+	ID      uuid.UUID `gorm:"type:uuid;primary_key;"`
+	Address string    `gorm:"unique;not null"`
+	Balance int       `gorm:"not null"`
+}
+
+func (wallet *Wallet) BeforeCreate(tx *gorm.DB) (err error) {
+	wallet.ID = uuid.New()
+	return
+}
+
+func InitializeWallet(db *gorm.DB, address string, initialBalance int) error {
+	var wallet Wallet
+	result := db.Where("address = ?", address).First(&wallet)
+	if result.Error == gorm.ErrRecordNotFound {
+		wallet = Wallet{
+			Address: address,
+			Balance: initialBalance,
+		}
+		return db.Create(&wallet).Error
+	}
+	return result.Error
 }
