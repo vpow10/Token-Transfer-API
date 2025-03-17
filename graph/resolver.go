a81a95b6c9ed29ee5c26e52a2d019679e16d8239
vpow10 <jan.szymczak10@gmail.com>
2025-03-17 10:22:35 +0100

    Fix resolver for graphQL after automated code generation

--- graph/resolver.go
+++ graph/resolver.go
@@ -4,6 +4,8 @@ package graph
 
 import (
 	"context"
+	"errors"
+	"token-transfer-api/db"
 	"token-transfer-api/graph/generated"
 	"token-transfer-api/models"
 )
@@ -12,32 +14,6 @@ type Resolver struct{}
 
 // Transfer is the resolver for the transfer field.
 func (r *mutationResolver) Transfer(ctx context.Context, fromAddress string, toAddress string, amount int) (*models.Wallet, error) {
-	panic("not implemented")
-}
-
-// ID is the resolver for the id field.
-func (r *walletResolver) ID(ctx context.Context, obj *models.Wallet) (string, error) {
-	panic("not implemented")
-}
-
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
-
-// Wallet returns generated.WalletResolver implementation.
-func (r *Resolver) Wallet() generated.WalletResolver { return &walletResolver{r} }
-
-type mutationResolver struct{ *Resolver }
-type walletResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	type Resolver struct{}
-func (r *Resolver) Transfer(ctx context.Context, fromAddress string, toAddress string, amount int) (*models.Wallet, error) {
 	if amount <= 0 {
 		return nil, errors.New("amount must be positive")
 	}
@@ -88,4 +64,17 @@ func (r *Resolver) Transfer(ctx context.Context, fromAddress string, toAddress s
 
 	return &fromWallet, nil
 }
-*/
+
+// ID is the resolver for the id field.
+func (r *walletResolver) ID(ctx context.Context, obj *models.Wallet) (string, error) {
+	return obj.ID.String(), nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Wallet returns generated.WalletResolver implementation.
+func (r *Resolver) Wallet() generated.WalletResolver { return &walletResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type walletResolver struct{ *Resolver }
